aa6ea0405410a03fdbe2bb065ac4ca7f55eea4ef
Dyaqi <1071054313@qq.com>
2021-05-07 16:43:59 +0800

    告白墙接口

---
+++ src/main/java/com/wfuhui/modules/help/controller/ConfessionController.java
@@ -0,0 +1,9 @@
+package com.wfuhui.modules.help.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("confession")
+public class ConfessionController {
+}
